1fc8b06c17c5a214cf7e68f48bb6a0def5305135
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-24 17:39:40 +0300

    token response and request

---
+++ src/main/java/org/project/authservice/auth/AuthResponse.java
@@ -0,0 +1,13 @@
+package org.project.authservice.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+    private Long user_id;
+}
