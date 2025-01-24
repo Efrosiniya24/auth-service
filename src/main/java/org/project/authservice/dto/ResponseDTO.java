f8cbaa63d480b25110155ce56713a84b16a73160
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-24 18:35:30 +0300

    add builder annotation

--- src/main/java/org/project/authservice/dto/ResponseDTO.java
+++ src/main/java/org/project/authservice/dto/ResponseDTO.java
@@ -1,12 +1,14 @@
 package org.project.authservice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ResponseDTO {
     private String accessToken;
     private Long user_id;
