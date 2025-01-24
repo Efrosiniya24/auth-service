f949f196a94d444b643deba0c90fea6ffaaeafaf
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-24 19:41:38 +0300

    sign In

--- src/main/java/org/project/authservice/service/AuthService.java
+++ src/main/java/org/project/authservice/service/AuthService.java
@@ -2,10 +2,14 @@ package org.project.authservice.service;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.project.authservice.dto.AuthenticateRequestDTO;
 import org.project.authservice.dto.RegistrationRequestDTO;
 import org.project.authservice.dto.ResponseDTO;
 import org.project.authservice.entity.UserEntity;
 import org.project.authservice.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +20,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public ResponseDTO signUp(RegistrationRequestDTO request) {
         UserEntity user = UserEntity
@@ -30,6 +35,25 @@ public class AuthService {
 
         return ResponseDTO.builder()
                 .accessToken(token)
+                .build();
+    }
+
+    public ResponseDTO signIn(AuthenticateRequestDTO request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        UserEntity user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException(request.getEmail()));
+
+        String token = jwtService.generateAccessToken(user);
+
+        return ResponseDTO.builder()
+                .accessToken(token)
+                .user_id(user.getId())
                 .build();
     }
 }
