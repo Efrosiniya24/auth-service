8e6fb4ddc5fbfa373fc9693d4b948593665b215f
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-24 19:29:01 +0300

    generate token

---
+++ src/main/java/org/project/authservice/service/JWTService.java
@@ -0,0 +1,34 @@
+package org.project.authservice.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.project.authservice.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class JWTService {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    public String generateAccessToken(UserEntity user) {
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
