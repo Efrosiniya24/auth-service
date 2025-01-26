baa792e8d54fa906fb7386d30dc2d9dcd8dca949
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-26 19:23:58 +0300

    signIn test

--- src/test/java/org/project/authservice/service/AuthServiceTest.java
+++ src/test/java/org/project/authservice/service/AuthServiceTest.java
@@ -5,11 +5,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.project.authservice.dto.AuthenticateRequestDTO;
 import org.project.authservice.dto.RegistrationRequestDTO;
 import org.project.authservice.dto.ResponseDTO;
 import org.project.authservice.entity.UserEntity;
 import org.project.authservice.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -29,14 +34,17 @@ class AuthServiceTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
+    @Mock
+    private AuthenticationManager authenticationManager;
+
     @InjectMocks
     private AuthService authService;
 
     @Test
-    void signUpTest(){
+    void signUpTest() {
         //given
         RegistrationRequestDTO requestDTO = new RegistrationRequestDTO("username", "password", "email");
-        UserEntity userEntity = new UserEntity(1L,"username", "encodedPassword", "email");
+        UserEntity userEntity = new UserEntity(1L, "username", "encodedPassword", "email");
 
         //when
         when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
@@ -50,6 +58,31 @@ class AuthServiceTest {
         verify(passwordEncoder, times(1)).encode("password");
         verify(userRepository, times(1)).save(any(UserEntity.class));
         verify(jwtService, times(1)).generateAccessToken(any(UserEntity.class));
+    }
+
+    @Test
+    void signIn() {
+        //given
+        String email = "test@test.com";
+        String token = "token";
+        AuthenticateRequestDTO request = new AuthenticateRequestDTO(email, "password");
+        UserEntity userEntity = new UserEntity(1L, "username", "password", email);
+
+        //when
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(userEntity));
+        when(jwtService.generateAccessToken(userEntity)).thenReturn(token);
+
+        //then
+        ResponseDTO response = authService.signIn(request);
+
+        assertEquals(token, response.getAccessToken());
+        assertEquals(1L, response.getUser_id());
+
+        verify(authenticationManager, times(1))
+                .authenticate(new UsernamePasswordAuthenticationToken(email, "password"));
+
+        verify(userRepository, times(1)).findByEmail(email);
+        verify(jwtService, times(1)).generateAccessToken(userEntity);
     }
 
 }
